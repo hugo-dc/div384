db6b12b3c3d24a03ebd452a7a4b76c2ccf7d4cd5
poemm <36397285+poemm@users.noreply.github.com>
2020-10-13 20:16:35 -0500

    .

---
+++ compile.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+const { compiler } = require('./huff/src');
+const parser = require('./huff/src/parser');
+
+const pathToData = path.posix.resolve(__dirname, './');
+
+const { inputMap, macros, jumptables } = parser.parseFile('main.huff', pathToData);
+
+const {
+    data: { bytecode: macroCode },
+//} = parser.processMacro('MILLER_LOOP_TEST_HARDCODED', 0, [], macros, inputMap, jumptables);
+} = parser.processMacro('MILLER_LOOP_BENCH', 0, [], macros, inputMap, jumptables);
+
+console.log("0x"+macroCode)
+
