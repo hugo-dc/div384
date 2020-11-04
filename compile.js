bf6d5444fb04aaa3f63e0613cc5ac37e3688edbd
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2020-11-04 15:36:46 -0600

    div384 test

--- compile.js
+++ compile.js
@@ -6,12 +6,12 @@ const parser = require('./huff/src/parser');
 
 const pathToData = path.posix.resolve(__dirname, './');
 
-const { inputMap, macros, jumptables } = parser.parseFile('main.huff', pathToData);
+const { inputMap, macros, jumptables } = parser.parseFile('div384.huff', pathToData);
 
 const {
     data: { bytecode: macroCode },
 //} = parser.processMacro('MILLER_LOOP_TEST_HARDCODED', 0, [], macros, inputMap, jumptables);
-} = parser.processMacro('MILLER_LOOP_BENCH', 0, [], macros, inputMap, jumptables);
+} = parser.processMacro('DIV384', 0, [], macros, inputMap, jumptables);
 
 console.log("0x"+macroCode)
 
